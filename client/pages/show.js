fe9913df483d1bd323654ff7808624b311142ff1
sungwoon95 <lad0305@naver.com>
2022-02-01 03:11:10 +0900

    FE: change fetcher

--- client/pages/show.js
+++ client/pages/show.js
@@ -1,18 +1,42 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-import {useRouter} from 'next/router'
+import React,{useState,useEffect, useRef} from 'react'
 import {dataFetcher} from '../dataFetcher'
+import APIfetcher from '../APIfetcher'
 import PageView from './components/layout/pageView'
+import PageDetail from './components/layout/pageDetail'
 import Sort from './components/layout/Sort'
-
+import useInfiniteScroll from './hooks/useInfiniteScroll'
+import loadContent from './hooks/loadContent'
 
 const Show = (props) => {
-  const {loadContent, isLoading} = dataFetcher("show",'1')
+  const [content, setContent] = useState([])
+  const [endScroll, setEndScroll] = useState(true)
+  const fetchMore = useRef(null)
+  const intersecting = useInfiniteScroll(fetchMore)
+  
+  const [pageType, setPageType] = useState(true);
 
+  const toggleView = () => {
+    setPageType(!pageType)
+  }
+
+  const getContent = async() => {
+    const newContents = await APIfetcher ('get','/show/content/scroll', {
+      params:{cursor: content[content.length - 1]?.id || ''}}
+      )
+      if (newContents.length === 0){
+        setEndScroll(false)
+      }
+    setContent(content=>[...content,...newContents])
+  }
+  useEffect (() => {
+    if(intersecting && endScroll) getContent()
+    console.log(intersecting)
+
+  },[intersecting,content])
   return(
-  <div className="container">
+    <div className="container">
     <Sort />
-    {isLoading 
+    {/* {isLoading 
     ?
     <p>Loadgin</p> 
     :
@@ -20,7 +44,18 @@ const Show = (props) => {
     {loadContent.map((item,idx)=>(
       <PageView key={item.id} ms={item} idx={idx+1}/>
     ))}
-    </>}    
+    </>}     */}
+    <button onClick={toggleView}>
+        {pageType? "자세히" :"일반"}
+      </button>
+    {pageType? content.map((item,idx)=>(
+      <PageDetail key={idx} ms={item} idx={idx+1}/>
+      )):
+      content.map((item,idx)=>(
+        <PageView key={idx} ms={item} idx={idx+1}/>
+        ))
+      }
+    <div ref={fetchMore} />
   </div>
   )
 }
