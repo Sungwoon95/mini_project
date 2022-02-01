88ddfd2952abf6da89a92c00f30b5947a76cb81f
sungwoon95 <lad0305@naver.com>
2022-02-01 20:49:07 +0900

    FE: header dark mode

--- client/pages/components/layout/Header.js
+++ client/pages/components/layout/Header.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React,{useState, useContext} from 'react';
 import {useRouter} from 'next/router'
 import Link from 'next/link'
+import {ThemeContext} from '../../contexts/theme.js'
 
+import Logo from '../../assets/logo.svg'
+import About from '../../assets/about.svg'
+import LightTheme from '../../assets/lightTheme.svg'
+import DarkTheme from '../../assets/darkTheme.svg'
 
 const Header = () => {
+  const [{isDark}, toggleTheme] =useContext(ThemeContext)
+
   return(
-    <div className="Header container">
+    <div className={`${isDark? 'Dark': 'Light'}_Header`}>
       <Link href="/">
         <div className="Header__logo">
           <div className="Header__logo--image">
+          <Logo className="Header__logo--image-svg"/>
           </div>
           <div className="Header__logo--title">
-            Hacker News
+            ReHacker<br />News
           </div>
         </div>
       </Link>
       <div className="Header__about">
-        <div className="Header__about--theme">
-          <div className="Header__about--theme-icons">
-            
-          </div>
+        <div className="Header__about--theme" onClick={toggleTheme}>
+            {isDark
+            ?<LightTheme className="Header__theme--image-svg"/>
+            :<DarkTheme className="Header__theme--image-svg"/>
+            }
         </div>
         <div className="Header__about--descript">
-        <span class="material-icons">
-help
-</span>
+          <About className="Header__about--image-svg"/>
         </div>
       </div>
     </div>
